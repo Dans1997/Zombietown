1929872f383eb15515dc2784e62ec714fe2354d5
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-06-16 15:30:50 -0300

    Changed Reticle and Added Muzzle Flash

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 1f;
+    [SerializeField] ParticleSystem muzzleFlashVFX;
 
     // Start is called before the first frame update
     void Start()
@@ -25,12 +27,23 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlashVFX.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             print("Player hit:  " + hit.transform.name);
             EnemyHealth enemyHealth = hit.transform.GetComponent<EnemyHealth>();
-            if (enemyHealth) 
+            if (enemyHealth)
             {
                 enemyHealth.ProcessHit(damage);
             }
