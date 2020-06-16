d4ed987a6f49832825c28ee53b9d54bf73575260
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-06-16 16:17:19 -0300

    Bullet Hit SFX

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 1f;
     [SerializeField] ParticleSystem muzzleFlashVFX;
+    [SerializeField] GameObject bulletHitVFX;
 
     // Start is called before the first frame update
     void Start()
@@ -41,7 +42,8 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            print("Player hit:  " + hit.transform.name);
+            GameObject gameObject = Instantiate(bulletHitVFX, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(gameObject, bulletHitVFX.GetComponentInChildren<ParticleSystem>().main.duration);
             EnemyHealth enemyHealth = hit.transform.GetComponent<EnemyHealth>();
             if (enemyHealth)
             {
