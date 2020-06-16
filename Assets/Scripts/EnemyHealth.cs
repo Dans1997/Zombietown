2b9866d9fa8831fa5b4df9f6f43e7103b63a1e57
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-06-16 14:33:22 -0300

    Implemented Raycast and Enemy Health

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float health = 3f;
+
+    public void ProcessHit(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            print("Enemy is dead");
+            // Play deathSFX?
+            // Play death animation?
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            print("Player hit:  " + hit.transform.name);
+            EnemyHealth enemyHealth = hit.transform.GetComponent<EnemyHealth>();
+            if (enemyHealth) 
+            {
+                enemyHealth.ProcessHit(damage);
+            }
+        }
+    }
+}
