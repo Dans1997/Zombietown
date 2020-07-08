4b9280b60b24f2a805c7ffe7cde40bc6a37f732c
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-07-08 17:10:03 -0300

    Implemented Weapon Zoom

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] float zoomedInSensitivity = 0.5f;
+
+    bool zoomedInToggle = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: Adapt this to different types of zooms
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (!zoomedInToggle)
+            {
+                GetComponent<Camera>().fieldOfView = zoomedInFOV;
+                GetComponentInParent<RigidbodyFirstPersonController>().mouseLook.XSensitivity = zoomedInSensitivity;
+                GetComponentInParent<RigidbodyFirstPersonController>().mouseLook.YSensitivity = zoomedInSensitivity;
+            }
+            else
+            {
+                GetComponent<Camera>().fieldOfView = zoomedOutFOV;
+                GetComponentInParent<RigidbodyFirstPersonController>().mouseLook.XSensitivity = zoomedOutSensitivity;
+                GetComponentInParent<RigidbodyFirstPersonController>().mouseLook.YSensitivity = zoomedOutSensitivity;
+            }
+            zoomedInToggle = !zoomedInToggle;
+        }
+    }
+}
