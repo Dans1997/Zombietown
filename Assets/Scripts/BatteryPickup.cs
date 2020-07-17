dceb13c7306fa3016993a62c48c95649241902b9
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-07-17 17:59:19 -0300

    Implemented FlashLight & Battery Pickup

---
+++ Assets/Scripts/BatteryPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickup : MonoBehaviour
+{
+    [SerializeField] float restoreAngle = 65f;
+    [SerializeField] float intensityAmount = 5f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FlashLight flashlight = FindObjectOfType<FlashLight>();
+        flashlight.RestoreLightAngle();
+        flashlight.AddLightIntensity(intensityAmount);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angleDecay = 0.1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light myLight = null;
+    float restoreAngle = 80f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!myLight) return;
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    public void RestoreLightAngle() { myLight.spotAngle = restoreAngle; }
+
+    public void AddLightIntensity(float intensityAmount) { myLight.intensity += intensityAmount; }
+
+    private void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle >= minimumAngle) myLight.spotAngle -= angleDecay;
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        if (myLight.intensity >= 0) myLight.intensity -= lightDecay;
+    }
+}
