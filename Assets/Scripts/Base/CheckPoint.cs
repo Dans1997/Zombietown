a32fa71b6466ec1801bbfcb7d7f4db8dc3f815e1
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-08-06 11:58:00 -0300

    Changes I Forgot to make

--- Assets/Scripts/Base/CheckPoint.cs
+++ Assets/Scripts/Base/CheckPoint.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class CheckPoint : MonoBehaviour
 {
     private static CheckPoint instance;
-    bool isActive = true;
+    bool isActive = false;
 
     // Awake is called before Start
     private void Awake()
